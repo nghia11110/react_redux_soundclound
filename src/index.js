04f98a9f6744b9093c20eb88519f0181feb48f38
Nghia NT <n.nghia@air-closet.com>
2018-06-05 19:17:48 +0900

    add redux structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TrackList from './components/TrackList';
+import { configureStore } from './store';
+import * as actions from './actions';
 
 const tracks = [
   {
@@ -12,6 +14,9 @@ const tracks = [
     title: 'Cơn mưa ngang qua'
   }
 ];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
   <TrackList tracks={tracks} />,
